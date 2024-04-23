c7b00173843c03d263d071ba0d3421a65ba74970
prajjwa-lit <prajjwal026@gmail.com>
2024-04-23 09:35:09 +0530

    added constants

---
+++ lib/utils/constants/colours/app_colours.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // static const Color primaryColor = ;
+  // static const Color secondaryColor = ;
+  // static const Color backgroundColor = ;
+  // static const Color textColor = ;
+}
